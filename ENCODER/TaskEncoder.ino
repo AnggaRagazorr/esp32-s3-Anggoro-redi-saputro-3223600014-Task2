844cd724f7ea3a581f6dbb08030d8fcdbb47eeaa
Anggororedii <anggororedi@gmail.com>
2025-11-12 06:41:05 +0700

    Update TaskEncoder.ino

--- ENCODER/TaskEncoder.ino
+++ ENCODER/TaskEncoder.ino
@@ -1,1 +1,29 @@
+#define CLK 10
+#define DT 11
+int lastStateCLK, counter = 0;
+
+void TaskEncoder(void *pvParameters) {
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  lastStateCLK = digitalRead(CLK);
+  while (1) {
+    int currentStateCLK = digitalRead(CLK);
+    if (currentStateCLK != lastStateCLK) {
+      if (digitalRead(DT) != currentStateCLK) counter++;
+      else counter--;
+      Serial.printf("Encoder Count: %d | Core: %d\n", counter, xPortGetCoreID());
+    }
+    lastStateCLK = currentStateCLK;
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Core 1
+  xTaskCreatePinnedToCore(TaskEncoder, "TaskEncoder_Core1", 2000, NULL, 4, NULL, 1);
+}
+
+void loop() {}
 
